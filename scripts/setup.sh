282d5e30faf3599485f830dbbddcd9cbbc5e535e
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-03-02 16:20:54 -0600

    Added setup script for docker containers

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker kill kmdb-api-server kmdb-postgres
+docker rm kmdb-api-server kmdb-postgres
+docker network rm kmdb-net
+
+docker pull postgres
+docker run --rm --name kmdb-postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+
+cd ../backend
+docker build -t kmdb-api-server:latest .
+docker run --rm -d -p 5000:5000 --name kmdb-api-server kmdb-api-server 
+
+docker network create kmdb-net
+docker network connect kmdb-net kmdb-postgres
+docker network connect kmdb-net kmdb-api-server
+
